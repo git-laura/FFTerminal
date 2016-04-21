afc7d0b00bb31063888b402184a702c7dfffcfa6
lmartinr <lmartinr@datatrends.es>
2016-04-21 16:49:40 +0200

    - Integrado el mapa en el main

--- app/src/main/java/ff/findyourfriend/activity/MapActivity.java
+++ app/src/main/java/ff/findyourfriend/activity/MapActivity.java
@@ -1,9 +1,20 @@
 package ff.findyourfriend.activity;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.SyncStateContract;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -18,10 +29,15 @@ import ff.findyourfriend.interfaces.UpdaterListener;
 /**
  * Created by laura on 09/04/2016.
  */
-public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapActivity extends FragmentActivity implements OnMapReadyCallback, android.location.LocationListener {
     private GoogleMap googleMap;
-    UpdaterListener updaterListener;
+    private UpdaterListener updaterListener;
+    private LocationManager locationManager;
     public static double lat = 40.300280;
+
+    // LocationManager
+    private static final long MIN_TIME = 400;
+    private static final float MIN_DISTANCE = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,17 +54,24 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
                 lat = value;
                 LatLng my_position = new LatLng(lat, -3.811728);
                 Log.d("Updating Coord", String.valueOf(value));
-                if(googleMap != null) {
+                if (googleMap != null) {
                     googleMap.clear();
                     googleMap.addMarker(new MarkerOptions().position(my_position).title("Marker in my_position"));
                     googleMap.moveCamera(CameraUpdateFactory.newLatLng(my_position));
+                    if (ActivityCompat.checkSelfPermission(getApplication().getBaseContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getBaseContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        return;
+                    }
+                    googleMap.setMyLocationEnabled(true);
                 }
             }
         };
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIME, MIN_DISTANCE, this);
+
         ReadCoordAsyncTask CoordRefreshThraed = new ReadCoordAsyncTask(updaterListener);
         CoordRefreshThraed.execute();
     }
-    
+
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -68,4 +91,29 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
         this.googleMap.moveCamera(CameraUpdateFactory.newLatLng(my_position));
     }
 
+    @Override
+    public void onLocationChanged(Location location) {
+        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 15);
+        googleMap.animateCamera(cameraUpdate);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        locationManager.removeUpdates(this);
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        // TODO Auto-generated method stub
+    }
 }
